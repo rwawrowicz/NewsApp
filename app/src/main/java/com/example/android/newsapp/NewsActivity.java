8fa3ee37ef59a43654d943b6eaa2e5e84394751e
Rafał Wawrowicz <rwawrowicz@gmail.com>
2018-07-17 20:54:38 +0200

    News App, Stage 2

--- app/src/main/java/com/example/android/newsapp/NewsActivity.java
+++ app/src/main/java/com/example/android/newsapp/NewsActivity.java
@@ -4,12 +4,16 @@ import android.app.LoaderManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -22,10 +26,10 @@ public class NewsActivity extends AppCompatActivity implements LoaderManager.Loa
 
     private static final int LOADER_ID = 1;
     private TextView emptyTextView;
-    NewsAdapter adapter;
-    ListView newsListView;
+    private NewsAdapter adapter;
+    private ListView newsListView;
     private static final String REQUEST_URL =
-            "https://content.guardianapis.com/search?q=netflix&order-by=newest&page-size=20&show-tags=contributor&api-key=test";
+            "https://content.guardianapis.com/search?q=netflix";
 
 
     @Override
@@ -46,7 +50,7 @@ public class NewsActivity extends AppCompatActivity implements LoaderManager.Loa
             ProgressBar progressBar = findViewById(R.id.loading_spinner);
             progressBar.setVisibility(View.GONE);
             TextView textView = findViewById(R.id.empty_view);
-            textView.setText("No internet connection!");
+            textView.setText(getResources().getString(R.string.no_internet_connection));
         }
 
     }
@@ -60,6 +64,7 @@ public class NewsActivity extends AppCompatActivity implements LoaderManager.Loa
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
+                //TODO: Handle 'no browser' scenario
                 Uri url = Uri.parse(newsToShow.get(i).getUrl());
                 Intent urlIntent = new Intent(Intent.ACTION_VIEW, url);
                 startActivity(urlIntent);
@@ -69,7 +74,31 @@ public class NewsActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public Loader<List<News>> onCreateLoader(int i, Bundle bundle) {
-        return new NewsLoader(this, REQUEST_URL);
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        // getString retrieves a String value from the preferences. The second parameter is the default value for this preference.
+        String pageSize = sharedPrefs.getString(
+                getString(R.string.settings_page_size_key),
+                getString(R.string.settings_page_size_default));
+
+        String orderBy  = sharedPrefs.getString(
+                getString(R.string.settings_order_by_key),
+                getString(R.string.settings_order_by_default)
+        );
+
+        // parse breaks apart the URI string that's passed into its parameter
+        Uri baseUri = Uri.parse(REQUEST_URL);
+
+        // buildUpon prepares the baseUri that we just parsed so we can add query parameters to it
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        // Append query parameter and its value. For example, the `format=geojson`
+        uriBuilder.appendQueryParameter("order-by", orderBy);
+        uriBuilder.appendQueryParameter("page-size", pageSize);
+        uriBuilder.appendQueryParameter("show-tags", "contributor");
+        uriBuilder.appendQueryParameter("api-key", "test");
+
+        return new NewsLoader(this, uriBuilder.toString());
     }
 
     @Override
@@ -86,6 +115,24 @@ public class NewsActivity extends AppCompatActivity implements LoaderManager.Loa
     @Override
     public void onLoaderReset(Loader<List<News>> loader) {
         adapter.clear();
+    }
+    @Override
+    // This method initialize the contents of the Activity's options menu.
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the Options Menu we specified in XML
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
